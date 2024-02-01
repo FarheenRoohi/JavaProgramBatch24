ae2bb45d46321c2c111d8f3382cddbc417532e47
Farheen Roohi <roohihabeeb04@gmail.com>
2024-01-31 20:05:43 -0600

    updated tree map java added new records

--- src/treeMapDemo/TreeMapDemo.java
+++ src/treeMapDemo/TreeMapDemo.java
@@ -11,6 +11,7 @@ public class TreeMapDemo {
 		mp.put(104, "farheen");
 		mp.put(106, "parveen");
 		mp.put(108, 30);
+		mp.put(109, "abc");
 	    System.out.println(mp);
 	   // System.out.println(mp.descendingMap());
 	    NavigableMap<Integer,Object> nm = mp.descendingMap();
